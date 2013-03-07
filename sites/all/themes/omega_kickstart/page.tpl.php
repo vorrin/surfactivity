bc1b9397cf8d4c06659f70e38a5c0e62c334eff8
Maurizio <vorrin.tal@gmail.com>
2013-03-07 18:57:49 +0000

    new commit

--- sites/all/themes/omega_kickstart/page.tpl.php
+++ sites/all/themes/omega_kickstart/page.tpl.php
@@ -6,19 +6,6 @@
 ?>
 
 
-
-
-
-<!--
-
-<div id='headercontainer'> 
-<div id='left'></div>
-<div id='right'></div>
-<div id='center'> SURFACTIVITY </div>
-
-</div>
--->
-
 <div<?php print $attributes; ?>>
   <?php if (isset($page['header'])) : ?>
 
@@ -29,12 +16,6 @@
     <?php print render($page['content']); ?>
   <?php endif; ?>  
 
-  <div id='guest-house-container'>
-      <div id='left'></div>
-      <div id='center'></div>
-      <div id='right'></div>
-  </div>
-  
   <?php if (isset($page['footer'])) : ?>
     <?php print render($page['footer']); ?>
   <?php endif; ?>
