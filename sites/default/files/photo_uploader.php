b641e98459f471e7f7a7f21712cce9321d968c4d
Maurizio <vorrin.tal@gmail.com>
2013-03-15 19:36:02 +0000

    and some cleanup and stuff

---
+++ sites/default/files/photo_uploader.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+
+<form action="upload_file.php" method="post" enctype="multipart/form-data">
+<label for="file">Filename:</label>
+<input type="file" name="file[]" id="file" accept="image/*" multiple="multiple"/>
+
+<br />
+<input type="submit" name="submit" value="Submit" />
+</form>
+
+</body>
+</html>
+
